56c9392d9473dd8e95745f43d48baa07d32249ab
Jan Stanley Watt <12198769+JanStanleyWatt@users.noreply.github.com>
2024-07-04 01:47:22 +0900

    feat: Add API endpoint for retrieving file information

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/JanStanleyWatt/planb/pkg/autogen/go/api/v1/apiv1connect"
+	"github.com/JanStanleyWatt/planb/server"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
 func main() {
-	// This is a comment
-	println("Hello, World!")
+	srv := &server.PlanBServiceServer{}
+	mux := http.NewServeMux()
+
+	path, handler := apiv1connect.NewPlanBClientToServiceHandler(srv)
+	mux.Handle(path, handler)
+
+	http.ListenAndServe(":8080", h2c.NewHandler(mux, &http2.Server{}))
 }
